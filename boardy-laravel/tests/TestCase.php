61a1e72d3300005b9505aac966b9ae74ffa7d39a
gorgeous <maks.24156@gmail.com>
2026-06-02 18:45:57 +0500

    fix testCase + add vite

--- boardy-laravel/tests/TestCase.php
+++ boardy-laravel/tests/TestCase.php
@@ -3,8 +3,15 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Vite; // Не забудь импортировать фасад
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Заставляет Laravel игнорировать отсутствие скомпилированного манифеста Vite
+        Vite::spy();
+    }
 }
